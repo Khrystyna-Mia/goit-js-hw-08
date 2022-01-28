d9957879e4e96aa581be075e13233f98aac2aa6b
Khrystyna-Mia <88985351+Khrystyna-Mia@users.noreply.github.com>
2022-01-28 14:16:54 +0000

    Deploying to gh-pages from @ Khrystyna-Mia/goit-js-hw-08@adbeb33909fbc1d76ad5fbe912ca13ed8948dc24 🚀

--- 02-video.f7ec224c.js
+++ 02-video.f7ec224c.js
@@ -7,4 +7,4 @@ var t=arguments[3],n="Expected a function",e=NaN,i="[object Symbol]",r=/^\s+|\s+
 },{}],"iiAF":[function(require,module,exports) {
 "use strict";var e=r(require("@vimeo/player")),t=r(require("lodash.throttle"));function r(e){return e&&e.__esModule?e:{default:e}}const o=document.querySelector("iframe"),u=new e.default(o);u.on("timeupdate",(0,t.default)(l,1e3));const a="videoplayer-current-time";function l({seconds:e}){localStorage.setItem(a,e)}function n(){localStorage.getItem(a)&&u.setCurrentTime(localStorage.getItem(a))}n();
 },{"@vimeo/player":"zU8P","lodash.throttle":"FNyO"}]},{},["iiAF"], null)
-//# sourceMappingURL=/goit-js-hw-08/02-video.9aee05a1.js.map
+//# sourceMappingURL=/goit-js-hw-08/02-video.f7ec224c.js.map
