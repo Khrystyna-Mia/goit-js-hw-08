b9052c823e353afccf2eb5fbecff8533553d9b5c
Khrystyna Hrekul <grekulkristi@gmail.com>
2022-01-28 13:38:17 +0200

    28.01

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+player.on('timeupdate', throttle(onPlay, 1000));
+
+const currentTime = "videoplayer-current-time";
+
+function onPlay({ seconds }) {
+    localStorage.setItem(currentTime, seconds)
+}
+
+setCurrentTime()
+function setCurrentTime() {
+    if (!localStorage.getItem(currentTime)) {
+        return;
+    }
+    player.setCurrentTime(localStorage.getItem(currentTime))
+ }
