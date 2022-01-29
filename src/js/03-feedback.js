c7d80465c8bb20678c658cb3d621c0a5949aec35
Khrystyna Hrekul <grekulkristi@gmail.com>
2022-01-29 18:24:09 +0200

    edited code

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,44 +2,37 @@ import throttle from "lodash.throttle";
 
 const LOCALSTORAGE_KEY = "feedback-form-state";
 
-const refs = {
-    form: document.querySelector('.feedback-form'),
-    textarea: document.querySelector('.feedback-form textarea')
- }
-
+const refForm = document.querySelector('.feedback-form');
+const refEmail = document.querySelector('input');
+const refTextarea = document.querySelector('textarea');
 const formData = {}
- 
-refs.form.addEventListener('submit', onFormSubmit);
-refs.form.addEventListener('input', throttle(onInputEntry, 500));
+
+refForm.addEventListener('input', throttle(onInputEntry, 500));
+refForm.addEventListener('submit', onFormSubmit);
+
 saveFormData();
 
-
-refs.form.addEventListener('input', evt => {
+function onInputEntry(evt) {
     formData[evt.target.name] = evt.target.value;
-});
+
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
+}
 
 function onFormSubmit(evt) {
     evt.preventDefault();
 
     console.log(JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)));
 
-    evt.target.reset();
+    evt.currentTarget.reset();
     localStorage.removeItem(LOCALSTORAGE_KEY);
-}
-
-function onInputEntry() {
-    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
 }
 
 function saveFormData() {
     const saveData = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
 
     if (saveData) {
-       form.elements.value = saveData.email ? saveData.email : saveData.value;
-       form.elements.value = saveData.message ? saveData.message : saveData.value;
-    }
-    // if (saveData) {
-    //     form.elements = saveData.email ?? saveData.value;
-    //     form.elements = saveData.message ?? saveData.value;
-    // }
+        refEmail.value = saveData.email;
+        refTextarea.value = saveData.message;
+    } 
 }
+   
